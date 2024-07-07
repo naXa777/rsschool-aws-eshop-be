ecf6d12994a4511b9c34addc71d17266e4b64f4e
Pavel <naxa@nextmail.ru>
2024-07-07 16:59:55 +0200

    feat: product-service as consumer (SQS) and producer (SNS)

---
+++ src/product-service/product_service/catalog_batch_process.py
@@ -0,0 +1,37 @@
+from aws_cdk import (
+    aws_lambda as _lambda,
+    aws_lambda_event_sources as lambda_event_sources,
+    aws_sqs as sqs,
+    aws_sns as sns,
+    aws_sns_subscriptions as subscriptions,
+    Stack,
+)
+from constructs import Construct
+
+
+class CatalogBatchProcess(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, environment, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        create_product_topic = sns.Topic(
+            self, 'CreateProductTopic', topic_name='createProductTopic'
+        )
+
+        create_product_topic.add_subscription(subscriptions.EmailSubscription('test@test.com'))
+        catalog_items_queue = sqs.Queue(self, 'CatalogItemsQueue', queue_name='catalogItemsQueue')
+        event_source = lambda_event_sources.SqsEventSource(catalog_items_queue, batch_size=5)
+
+        environment['SNS_TOPIC_ARN'] = create_product_topic.topic_arn
+
+        self.catalog_batch_process = _lambda.Function(
+            self, 'CatalogBatchProcessHandler',
+            runtime=_lambda.Runtime.PYTHON_3_11,
+            code=_lambda.Code.from_asset('product_service/lambda_func/'),
+            handler='catalog_batch_process.handler',
+            environment=environment
+        )
+
+        self.catalog_batch_process.add_event_source(event_source)
+        catalog_items_queue.grant_consume_messages(self.catalog_batch_process)
+        create_product_topic.grant_publish(self.catalog_batch_process)
---
+++ src/product-service/product_service/lambda_func/catalog_batch_process.py
@@ -0,0 +1,90 @@
+import json
+import os
+import boto3
+import uuid
+import traceback
+
+dynamodb = boto3.resource('dynamodb')
+sns_client = boto3.resource('sns')
+
+def handler(event, context):
+    // print("POST /product request. Body: %s", event.get('body'))
+    products_for_sns = []
+    sns_topic_arn = os.environ['SNS_TOPIC_ARN']
+    for record in event['Records']
+        product_data = json.loads(record['body'])
+        dynamodb = boto3.client('dynamodb', 'eu-central-1')
+        stocks_table_name = os.getenv('STOCKS_TABLE_NAME')
+        products_table_name = os.getenv('PRODUCTS_TABLE_NAME')
+
+        required_fields = ['title', 'price', 'count']
+
+        for field in required_fields:
+            if field not in product_data:
+                return {
+                    'statusCode': 400,
+                    'body': json.dumps({'error': f'{field} is required'})
+                }
+
+        product_id = str(uuid.uuid4())
+
+        product_item = {
+            'Put': {
+                'TableName': products_table_name,
+                'Item': {
+                    'id': {'S': product_id},
+                    'title': {'S': product_data['title']},
+                    'description': {'S': product_data.get('description', '')},
+                    'price': {'N': str(product_data['price'])},
+                }
+            }
+        }
+        stock_item = {
+            'Put': {
+                'TableName': stocks_table_name,
+                'Item': {
+                    'product_id': {'S': product_id},
+                    'count': {'N': str(product_data['count'])}
+                }
+            }
+        }
+
+        # Perform a transaction write
+        dynamodb.transact_write_items(
+            TransactItems=[product_item, stock_item]
+        )
+
+        sns_product = {
+            'id': product_id,
+            'title': product_data['title'],
+            'description': product_data.get('description', ''),
+            'price': product_data['price'],
+            'count': product_data['count'],
+        }
+        products_for_sns.append(sns_product)
+
+    sns_message = {
+        'default': json.dumps({
+            'message': 'Products created successfully',
+            'products': products_for_sns
+        })
+    }
+    response = sns_client.publish(
+        TopicArn=sns_topic_arn,
+        Message=json.dumps(sns_message),
+        MessageStructure='json'
+    )
+
+    print(f"Message sent to SNS topic: {response['MessageId']}")
+
+    return {
+        'statusCode': 200,
+        "headers": {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+            "content-type": "application/json"
+        },
+        'body': json.dumps({
+            'message': 'Batch processed successfully'
+        })
+    }
