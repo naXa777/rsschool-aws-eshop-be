668a1e86f0653090a95fad0ad07cc2d18ba89355
Pavel <naxa@nextmail.ru>
2024-07-12 19:33:00 +0200

    feat: integrate import-service with authorizer-service

--- src/import-service/import_service/api_gateway.py
+++ src/import-service/import_service/api_gateway.py
@@ -10,11 +10,22 @@ class ApiGateway(Stack):
 
     def __init__(self, scope: Construct, construct_id: str,
                  import_products_fn: _lambda,
-                 **kwargs
-                 ) -> None:
+                 **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        basic_auth_lambda = _lambda.Function.from_function_name(self, "authFunction", "AuthFunction")
+
         api = _api.RestApi(self, 'ImportServiceApi', rest_api_name='Import Service')
+        authorizer = _api.TokenAuthorizer(
+            self, 'BasicAuthorizer',
+            handler=basic_auth_lambda,
+            identity_source='method.request.header.Authorization'
+        )
 
         import_resource = api.root.add_resource('import')
-        import_resource.add_method('GET', _api.LambdaIntegration(import_products_fn))
+        import_resource.add_method(
+            'GET',
+            _api.LambdaIntegration(import_products_fn),
+            authorization_type=_api.AuthorizationType.CUSTOM,
+            authorizer=authorizer
+        )
