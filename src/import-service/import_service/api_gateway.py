4f4ef1d62aee35c13fa5c6fbc5ecb94b33b40355
Pavel <naxa@nextmail.ru>
2024-07-15 00:43:39 +0200

    fix: CORS and response body for auth errors

--- src/import-service/import_service/api_gateway.py
+++ src/import-service/import_service/api_gateway.py
@@ -15,11 +15,45 @@ class ApiGateway(Stack):
 
         basic_auth_lambda = _lambda.Function.from_function_name(self, "authFunction", "AuthFunction")
 
-        api = _api.RestApi(self, 'ImportServiceApi', rest_api_name='Import Service')
+        api = _api.RestApi(
+            self, 'ImportServiceApi',
+            default_cors_preflight_options={
+                "allow_origins": _api.Cors.ALL_ORIGINS,
+                "allow_methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+                "allow_headers": _api.Cors.DEFAULT_HEADERS,
+            },
+            rest_api_name='Import Service'
+        )
         authorizer = _api.TokenAuthorizer(
             self, 'BasicAuthorizer',
             handler=basic_auth_lambda,
             identity_source='method.request.header.Authorization'
+        )
+        api.add_gateway_response(
+            "UnauthorizedResponse",
+            type=_api.ResponseType.UNAUTHORIZED,
+            response_headers={
+                "Access-Control-Allow-Origin": "'*'",
+                "Access-Control-Allow-Headers": "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+                "Access-Control-Allow-Methods": "'GET, POST, PUT, DELETE, OPTIONS'",
+            },
+            status_code="401",
+            templates={
+                "application/json": '{"message": "Unauthorized"}'
+            },
+        )
+        api.add_gateway_response(
+            "AccessForbiddenResponse",
+            type=_api.ResponseType.ACCESS_DENIED,
+            response_headers={
+                "Access-Control-Allow-Origin": "'*'",
+                "Access-Control-Allow-Headers": "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+                "Access-Control-Allow-Methods": "'GET, POST, PUT, DELETE, OPTIONS'",
+            },
+            status_code="403",
+            templates={
+                "application/json": '{"message": "Access Forbidden"}'
+            },
         )
 
         import_resource = api.root.add_resource('import')
