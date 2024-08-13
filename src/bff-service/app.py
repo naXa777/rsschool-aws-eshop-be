acaba0cc523202d6767240866ff165ceab4c7220
Pavel <naxa@nextmail.ru>
2024-08-13 21:15:02 +0200

    fix: stream response from cart service

--- src/bff-service/app.py
+++ src/bff-service/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, Response, stream_with_context
 import requests
 from os import getenv
 from dotenv import load_dotenv
@@ -10,25 +10,17 @@ app = Flask(__name__)
 
 @app.route('/<service_name>', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def proxy(service_name):
-    # Fetch the service URL from the environment variables
     service_url = getenv(service_name)
-
     if not service_url:
-        return jsonify({"error": "Cannot process request"}), 502
+        return Response("Cannot process request", status=502)
 
-    # Append the original query string to the service URL
     full_url = f"{service_url}?{request.query_string.decode()}"
+    response = requests.get(full_url, stream=True)
 
-    # Forward the request to the actual service
-    response = requests.request(
-        method=request.method,
-        url=full_url,
-        headers={key: value for key, value in request.headers if key != 'Host'},
-        data=request.get_data(),
-        allow_redirects=False)
-
-    # Return the response received from the service
-    return (response.content, response.status_code, response.headers.items())
+    def generate():
+        for chunk in response.iter_content(chunk_size=4096):
+            yield chunk
+    return Response(stream_with_context(generate()), status=response.status_code, content_type=response.headers['Content-Type'])
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
